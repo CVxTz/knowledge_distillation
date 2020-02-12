d094054673894fa1e9c5f941860211fcea1bd42a
jenazzad <mansaryouness@gmail.com>
2020-02-12 22:33:41 +0100

    add ensemble har model

--- code/run.sh
+++ code/run.sh
@@ -1,5 +1,6 @@
-for i in {0..15}
+for i in {0..5}
 do
 python baseline_har.py > baseline_har.txt
-python permado_har.py > permado_har.txt
+python ensemble_har.py > ensemble_har.txt
+#python permado_har.py > permado_har.txt
 done
