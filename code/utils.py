3cfd126612cac6e0fb28600a849f92fa07b65a3f
Jenazzad <mansaryouness@gmail.com>
2020-02-14 17:21:53 +0100

    Sample from Beta distribution once every epoch

--- code/utils.py
+++ code/utils.py
@@ -10,10 +10,11 @@ def gen(X, Y, batch_size=64):
     indexes = list(range(X.shape[0]))
     while True:
         shuffle(indexes)
-        for batch_indexes_1, batch_indexes_2 in zip(
-            chunker(indexes, size=batch_size), chunker(indexes[::-1], size=batch_size)
+        alphas = np.random.beta(0.2, 0.2, size=X.shape[0]).tolist()
+        for batch_indexes_1, batch_indexes_2, alphas_batch in zip(
+            chunker(indexes, size=batch_size), chunker(indexes[::-1], size=batch_size),
+                chunker(alphas, size=batch_size)
         ):
-            alphas = np.random.beta(0.2, 0.2, size=batch_size).tolist()
 
             X_1 = [X[i, ...] for i in batch_indexes_1]
             X_2 = [X[i, ...] for i in batch_indexes_2]
@@ -21,8 +22,8 @@ def gen(X, Y, batch_size=64):
             Y_1 = [Y[i, ...] for i in batch_indexes_1]
             Y_2 = [Y[i, ...] for i in batch_indexes_2]
 
-            X_batch = [l * a + (1 - l) * b for a, b, l in zip(X_1, X_2, alphas)]
+            X_batch = [l * a + (1 - l) * b for a, b, l in zip(X_1, X_2, alphas_batch)]
 
-            Y_batch = [l * a + (1 - l) * b for a, b, l in zip(Y_1, Y_2, alphas)]
+            Y_batch = [l * a + (1 - l) * b for a, b, l in zip(Y_1, Y_2, alphas_batch)]
 
             yield np.array(X_batch), np.array(Y_batch)
