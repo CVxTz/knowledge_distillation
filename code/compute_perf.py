405b8b71549cacbb465536644ba3acd9889732c0
jenazzad <mansaryouness@gmail.com>
2020-02-15 10:36:07 +0100

    remove dropout perf script

---
+++ code/compute_perf.py
@@ -0,0 +1,13 @@
+import json
+from glob import glob
+import numpy as np
+
+
+if __name__ == "__main__":
+    base_path = "../output/ecg/"
+    for name in ['baseline', 'kd', 'small']:
+        files =  glob(base_path+name+"*")
+        f1 = [json.load(open(x))['f1'] for x in files]
+        mean_f1 = np.mean(f1)
+        std_f1 = np.std(f1)
+        print(name, "f1", "%s +- %s"%(mean_f1, std_f1))
--- code/model.py
+++ code/model.py
@@ -11,22 +11,18 @@ from tensorflow.keras.layers import (
 )
 
 
-def get_model(do_rate=0.2, n_class=5):
+def get_model(n_class=5):
     inp = Input(shape=(187, 1))
     x = Convolution1D(64, kernel_size=5, activation=activations.relu, padding="valid")(
         inp
     )
     x = MaxPool1D(pool_size=4)(x)
-    x = Dropout(rate=do_rate)(x)
     x = Convolution1D(64, kernel_size=3, activation=activations.relu, padding="valid")(
         x
     )
     x = MaxPool1D(pool_size=4)(x)
-    x = Dropout(rate=do_rate)(x)
     x = GlobalMaxPool1D()(x)
-    x = Dropout(rate=do_rate)(x)
     x = Dense(64, activation=activations.relu)(x)
-    x = Dropout(rate=do_rate)(x)
     x = Dense(n_class, activation=activations.softmax)(x)
 
     model = models.Model(inputs=inp, outputs=x)
@@ -37,22 +33,18 @@ def get_model(do_rate=0.2, n_class=5):
     return model
 
 
-def get_small_model(do_rate=0.2, n_class=5):
+def get_small_model(n_class=5):
     inp = Input(shape=(187, 1))
     x = Convolution1D(32, kernel_size=5, activation=activations.relu, padding="valid")(
         inp
     )
     x = MaxPool1D(pool_size=4)(x)
-    x = Dropout(rate=do_rate)(x)
     x = Convolution1D(32, kernel_size=3, activation=activations.relu, padding="valid")(
         x
     )
     x = MaxPool1D(pool_size=4)(x)
-    x = Dropout(rate=do_rate)(x)
     x = GlobalMaxPool1D()(x)
-    x = Dropout(rate=do_rate)(x)
     x = Dense(16, activation=activations.relu)(x)
-    x = Dropout(rate=do_rate)(x)
     x = Dense(n_class, activation=activations.softmax)(x)
 
     model = models.Model(inputs=inp, outputs=x)
